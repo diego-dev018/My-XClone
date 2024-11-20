a5ff634aee03f0ce1ec885373a2df37c81be9d33
Diego <diegosanchez.dev018@gmail.com>
2024-11-19 21:59:00 -0600

    Add-Tweets:Data-Ts

---
+++ src/data/tweets.ts
@@ -0,0 +1,9 @@
+export const tweets = [
+    {
+        name: 'Elon Musk',
+        username: '@elon',
+        tweet: 'Saludos a todos!, Saludos',
+        time: '1h',
+        avatar: '/elon.jpg'
+    },
+]
